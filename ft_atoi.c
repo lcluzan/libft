7f7519abef132a78cc4730c266f3fb86f0bcbb75
lcluzan <lcluzan@student.42.fr>
2024-05-28 10:42:01 +0200

    modit ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,25 +6,25 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:35:58 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:36:29 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/28 10:40:31 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int i;
-	long result;
-	int sign;
+	int	i;
+	long	result;
+	int	sign;
 
 	result = 0;
 	sign = 1;
 	i = 0;
 	if (str[i] == '\0')
 		return (0);
-	while (str[i] == '\n' || str[i] == ' ' || str[i] == '\t' ||
-		   str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+	while (str[i] == '\n' || str[i] == ' ' || str[i] == '\t'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
 		if (str[i++] == '-')
