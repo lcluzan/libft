d69651de97e7231b2bbe739ccc6a6d14b3ff63a0
lcluzan <lcluzan@student.42.fr>
2024-05-29 14:50:52 +0200

    fix + norm ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nouillebobby <nouillebobby@student.42.f    +#+  +:+       +#+        */
+/*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 14:20:36 by nouillebobb       #+#    #+#             */
-/*   Updated: 2024/05/28 14:21:18 by nouillebobb      ###   ########.fr       */
+/*   Updated: 2024/05/29 11:55:23 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,5 @@ void	ft_lstadd_back(t_list **alst, t_list *new)
 	if (*alst)
 		ft_lstlast(*alst)->next = new;
 	else
+		*alst = new;
 }
