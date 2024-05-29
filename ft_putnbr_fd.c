0f6853c29b187b18af298f16e6bc976325069cb8
lcluzan <lcluzan@student.42.fr>
2024-05-29 15:05:09 +0200

    fix + norm ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nouillebobby <nouillebobby@student.42.f    +#+  +:+       +#+        */
+/*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:50:55 by nouillebobb       #+#    #+#             */
-/*   Updated: 2024/05/28 13:53:20 by nouillebobb      ###   ########.fr       */
+/*   Updated: 2024/05/29 15:04:38 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 static void	print_nb(long nb, int fd)
 {
