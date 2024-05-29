eb47e81d322aa6b701572eb092479faf674c4c22
lcluzan <lcluzan@student.42.fr>
2024-05-29 15:02:59 +0200

    fix + norm ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,15 +6,15 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 17:20:30 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:08:12 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/29 15:02:31 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	char *str;
+	char		*str;
 
 	str = b;
 	while (len)
