7749acc4ff4ee4b7587c33de9014ac20f7a6dcc9
lcluzan <lcluzan@student.42.fr>
2024-05-29 14:48:05 +0200

    fix + norm ft_isdigit

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,13 +6,13 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 17:15:57 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:08:56 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/29 14:47:05 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int	ft_isdigit(int c)
 {
-	return(c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
