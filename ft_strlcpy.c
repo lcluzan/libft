6f062d832fa62f22ed15734e35cf0646e373f1d3
lcluzan <lcluzan@student.42.fr>
2024-05-29 15:12:23 +0200

    fix + norm ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,11 +6,11 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 17:49:26 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:06:06 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/29 15:11:43 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
