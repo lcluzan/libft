479d857dc2b68122d51c244bea69cf58c8b9ba58
lcluzan <lcluzan@student.42.fr>
2024-05-29 15:07:41 +0200

    fix + norm ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,13 +6,13 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:38:29 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:39:02 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/29 15:07:18 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-char *ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
 	{
