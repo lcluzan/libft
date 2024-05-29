0b84a1a49a4622d27e580bd9be5abbfd345722f3
lcluzan <lcluzan@student.42.fr>
2024-05-29 15:15:26 +0200

    fix + norm ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,9 +6,11 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 17:49:23 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:14:32 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/29 15:14:57 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_strlen(char *str)
 {
