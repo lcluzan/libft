5687f7d4d555c208cb473f91ded55c8cdc239bbf
lcluzan <lcluzan@student.42.fr>
2024-05-29 15:21:50 +0200

    fix + norm ft_toupper

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,13 +6,13 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 16:29:16 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:30:12 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/29 15:21:25 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-int ft_toupper(int c)
+int	ft_toupper(int c)
 {
 	if (ft_islower(c))
 		return (c - 32);
