cd24c38fbf517f68b0893086b189942b7032e7ca
lcluzan <lcluzan@student.42.fr>
2024-05-29 14:46:01 +0200

    fix + norm ft_isascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,13 +6,13 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 17:49:16 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/05/23 16:09:06 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/05/29 14:45:35 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
 int	ft_isascii(int c)
 {
-	return(c >= 0 && c <= 127);
+	return (c >= 0 && c <= 127);
 }
