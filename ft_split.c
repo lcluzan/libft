b9eea4b1e72a8089aa2c552bd4f45f221494808c
lcluzan <lcluzan@student.42.fr>
2024-06-05 11:50:08 +0200

    norm ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:56:15 by nouillebobb       #+#    #+#             */
-/*   Updated: 2024/06/05 11:46:47 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/06/05 11:49:46 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    free_split(char **str)
+void	free_split(char **str)
 {
 	int	i;
 
